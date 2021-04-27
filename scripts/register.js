d4e173d9ccdd0eaa5cf93928e1c70c946aa90170
jeremylee-02 <s3752975@rmit.edu.vn>
2021-04-27 21:04:24 +0700

    Updated validate email

--- scripts/register.js
+++ scripts/register.js
@@ -6,11 +6,32 @@ function verify_pass(){
     var rePass = qs("#reupassword").value;
     var pass = qs("#upassword").value;
 
-     // Re type password match password
+     // Verifying
     if (rePass!= pass){
-        qs(".alert").innerHTML = "Password does not match";
+        qs(".alert").innerHTML = "Password does not match!";
     }else{
-        alert("Password is match")
+        qs(".alert").innerHTML = "Password is match!";
     }
+
+}
+    // Emailll
+function validateEmail(){
+    button = document.querySelector("#verify");
+    button.addEventListener("click", function(ev) {
+        pattern = new RegExp(/^[1-9A-Za-z^<>()[\]\\,;:\s@\"]|.?(?=.){3,}@[1-9A-Za-z]{1,}.?(?=.)[1-9A-Za-z]{3,}$/)
+        value = document.querySelector("#uemail").value;
+        if (pattern.test(value)){
+            qs("#alert_email").innerHTML = "Valid Email"
+        }else{
+            qs("#alert_email").innerHTML = "Invalid Email"
+        }
+
+
+
+        // name_pattern = new RegExp(/^[1-9]|[A-Z]|[a-z][1-9][A-Z][a-z][^<>()[\]\\,;:\s@\"]|[.]?{3,}$/);
+        // domain_pattern = new RegExp(/^@[]$/)
+    })
+
+
 }
 
