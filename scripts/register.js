c0800b13caa5e36abe6185e788194f1a2d1d19f7
jeremylee-02 <s3752975@rmit.edu.vn>
2021-04-29 22:09:59 +0700

    Updated validation

--- scripts/register.js
+++ scripts/register.js
@@ -1,37 +1,43 @@
 function qs(selector){
     return document.querySelector(selector)
 }
-
-function verify_pass(){
+// password validation
+function passwordValidation(){
     var rePass = qs("#reupassword").value;
     var pass = qs("#upassword").value;
-
-     // Verifying
-    if (rePass!= pass){
-        qs(".alert").innerHTML = "Password does not match!";
-    }else{
-        qs(".alert").innerHTML = "Password is match!";
-    }
-
+    button = document.querySelector("#verify");
+    button.addEventListener("click", function(ev){
+        pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+        value = document.querySelector("#upassword").value;
+        if(pattern.test(value)|| rePass == pass){
+            qs("#alert").innerHTML = "Valid passsword"
+        }else{
+            qs("#alert").innerHTML= "Invalid Password"
+        }
+    })
 }
+
     // Emailll
 function validateEmail(){
     button = document.querySelector("#verify");
     button.addEventListener("click", function(ev) {
-        pattern = new RegExp(/^[1-9A-Za-z^<>()[\]\\,;:\s@\"]|.?(?=.){3,}@[1-9A-Za-z]{1,}.?(?=.)[1-9A-Za-z]{3,}$/)
+        pattern = new RegExp(/^[1-9]|[A-Za-z]|[-_,;^*&].?(?=.){3,}@[1-9A-Za-z]{1,}.?(?=.)[1-9A-Za-z]{3,}$/)
         value = document.querySelector("#uemail").value;
         if (pattern.test(value)){
             qs("#alert_email").innerHTML = "Valid Email"
         }else{
             qs("#alert_email").innerHTML = "Invalid Email"
         }
-
-
-
         // name_pattern = new RegExp(/^[1-9]|[A-Z]|[a-z][1-9][A-Z][a-z][^<>()[\]\\,;:\s@\"]|[.]?{3,}$/);
         // domain_pattern = new RegExp(/^@[]$/)
     })
-
-
 }
-
+function personalinfo(){
+    button = document.querySelector();
+    button.addEventListener("click", function(ev){
+        pattern_name = new RegExp(/^[A-Z][a-z]{2,}$/);
+        pattern_address = new RegExp(/^[1-9][A-Z][a-z]{2,}/);
+        value1 = document.querySelector("#fname,#lname").value;
+        value2 = document.querySelector("#address").value;
+    })
+}
