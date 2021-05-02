f5a5641ef14f39d40db2e3cf677db4d6bee975f1
jeremy-lee02 <s3752975@rmit.edu.vn>
2021-05-02 23:28:44 +0700

    Created an overlay window

---
+++ scripts/overlay.js
@@ -0,0 +1,23 @@
+
+function on() {
+    document.querySelector("#overlay").style.display = "block";
+}
+function on1(){
+    document.querySelector("#overlay2").style.display = "block";
+
+}
+function on2(){
+    document.querySelector("#overlay3").style.display = "block";
+
+}
+function on3(){
+    document.querySelector("#overlay4").style.display = "block";
+}
+
+function off() {
+    document.querySelector(".overlay").style.display = "none";
+    document.querySelector("#overlay2").style.display = "none";
+    document.querySelector("#overlay3").style.display = "none";
+    document.querySelector("#overlay4").style.display = "none";
+}
+  
