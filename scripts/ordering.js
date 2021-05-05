30d6e003d354a4bd07437a6d6dc7232164daccc5
Ky Thanh <something2805@gmail.com>
2021-05-05 16:13:04 +0700

    "Add" button create/ update a local storage array

--- scripts/ordering.js
+++ scripts/ordering.js
@@ -3,11 +3,12 @@ function qs(selector){
 }
 
 var add_button = qs("#add_cart");
+var add_msg = qs("#add_msg");
 
 // code snippet by Elias Zamaria
 // https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript/2901298#2901298
 function numberWithCommas(num) {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
 var products = [
@@ -56,7 +57,7 @@ function fill_table(){
       
       row.innerHTML = `<tr>
       <td>${name}</td>
-      <td><input type="number" value="${quant}" onchange="get_quant()"</td>
+      <td><input type="number" value="${quant}" onchange="update_quant()"</td>
       <td>${price}</td>
       </tr>`
 
@@ -66,7 +67,7 @@ function fill_table(){
 }
 
 
-function get_quant(){
+function update_quant(){
   let new_prod = [];
   for (let r = 1; r <= cart_table.rows.length - 1; r++){
     new_prod.push({
@@ -84,4 +85,56 @@ function get_quant(){
   fill_table();
 }
 
-cart_table.addEventListener("DOMContentLoaded", fill_table())
+if(cart_table != null) {
+  cart_table.addEventListener("DOMContentLoaded", fill_table())
+}
+
+if(add_button != null) {
+  // This is the timer for the status msg after clicking add
+  var blink;
+  add_button.addEventListener("click", function add_cart(){
+      let prod_name = qs("#product_name").innerHTML;
+      let prod_price = qs("#product_price").innerHTML.replaceAll(".", "");
+      if (localStorage.product) {
+        let items = JSON.parse(localStorage.product);
+        let contain = false;
+        for (item of items){
+          if (item.name == prod_name){
+            item.quantity++;
+            contain = true;
+            break;
+          }
+        }
+        if(!contain){
+          items.push({
+            "name": prod_name,
+            "quantity": 0,
+            "price": prod_price
+          })
+        }
+        localStorage.product = JSON.stringify(items);
+      }
+      else {
+        let items = [];
+        items.push({
+          "name": prod_name,
+          "quantity": 0,
+          "price": prod_price
+        })
+        localStorage.product = JSON.stringify(items);
+        }
+      add_msg.style.display = "inline-block";
+      add_msg.innerText = `${prod_name} added to cart`
+      clearInterval(blink);
+      var timesRun = 0;
+      blink = setInterval(function(){
+          timesRun += 1;
+          if(timesRun == 6){
+            add_msg.style.display = "";
+            clearInterval(blink);
+          }
+          add_msg.style.opacity = (add_msg.style.opacity == 0.5) ? 1 : 0.5;
+        }, 400);
+    }
+  )
+}
