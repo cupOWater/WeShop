eef55f67223758ef59aae53e1d3d8a021f8f4c0b
jeremylee-02 <s3752975@rmit.edu.vn>
2021-05-05 16:06:01 +0700

    Change scrolling speed

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 setInterval(function(){
   auto_scroll = document.querySelector(".scroll");
   auto_scroll.scrollBy(2,0);
-},10);
+},15);
 
 setInterval(function(){
   auto_scroll = document.querySelector(".scroll1");
   auto_scroll.scrollBy(2,0);
-},10);
+},15);
